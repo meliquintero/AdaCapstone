e1611eabc0574614042b3b3bd25c36707dcaaa30
melissajimison <melissajimison@gmail.com>
2016-08-19 11:07:34 -0700

    still not sure of the role of this guy

--- api/models/User.js
+++ api/models/User.js
@@ -1,23 +1,67 @@
-/**
- * User.js
- *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
- */
-
+var bcrypt = require('bcrypt');
 module.exports = {
-
   attributes: {
+    uid: {
+      type: 'string',
+      unique: true
+    },
+    provider: {
+      type: 'string',
+    },
+    givenName: {
+      type: 'string',
+    },
+    familyName: {
+      type: 'string',
+    },
+    email: {
+      type: 'string',
+      unique: true
+    },
+    // password: {
+    //   type: 'string',
+    // },
+    photo: {
+      type: 'string',
+    },
+    toJSON: function() {
+      // this gives you an object with the current values
+      var obj = this.toObject();
+      // Remove the password object value
+      delete obj.password;
+      // return the new object without password
+      return obj;
+    }
+  },
 
-    name : { type: 'string' },
+  // create: function(profile){
+  //   User.create({
+  //       uid: profile.id,
+  //       provider: profile.provider,
+  //       email: profile.emails,
+  //       photo:profile.photos,
+  //       givenName: profile.name,
+  //       familyName: profile.displayName,
+  //   }).exec(function createCB(err, created){
+  //     return created
+  //   })
+  //
+  //   console.log( profile );
+  //   sails.log.debug(profile);
+  //   return profile.id
+  // },
 
-    email : { type: 'string' },
-
-    photo : { type: 'string' },
-
-    uid : { type: 'string' },
-
-    proider : { type: 'string' }
+  beforeCreate: function(user, cb) {
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) {
+          console.log(err);
+          cb(err);
+        }else{
+          user.password = hash;
+          cb(null, user);
+        }
+      });
+    });
   }
 };
-
