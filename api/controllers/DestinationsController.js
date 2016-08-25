8ffee30a41f5ef177054da4551adffbd36558ffa
melissajimison <melissajimison@gmail.com>
2016-08-25 13:20:25 -0700

    added a JQCalendar

--- api/controllers/DestinationsController.js
+++ api/controllers/DestinationsController.js
@@ -6,8 +6,10 @@ module.exports = {
 	index: function (req, res) {
     var Origin1 = req.body.origin_one
     var Origin2 = req.body.origin_two
-    var DepDate = req.body.departure_date
-    var RetDate = req.body.return_date
+    var DepDate = req.body.from.slice(6,10) +  "-" + req.body.from.slice(0,2) + "-" + req.body.from.slice(3,5)
+    var RetDate = req.body.to.slice(6,10) + "-" + req.body.to.slice(0,2) + "-" + req.body.to.slice(3,5)
+		console.log("DepDate", DepDate);
+		console.log("RetDate", RetDate);
 
 		destinationRequests.matchedDestinations(Origin1, Origin2, DepDate, RetDate).then(function(destinationsresult){
 				return res.view('index', {
